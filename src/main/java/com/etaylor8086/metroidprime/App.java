2ac525f2cc92d561629e2f839e7410bebf72c3be
etaylor8086 <etaylor8086@gmail.com>
2017-05-29 11:36:36 -0700

    Error handling

--- src/main/java/com/etaylor8086/metroidprime/App.java
+++ src/main/java/com/etaylor8086/metroidprime/App.java
@@ -1,7 +1,7 @@
 package com.etaylor8086.metroidprime;
 
 import com.beust.jcommander.JCommander;
-
+import com.beust.jcommander.ParameterException;
 import com.etaylor8086.metroidprime.randomizer.Randomizer;
 
 /**
@@ -13,20 +13,38 @@ public class App
 {
     public static void main( String[] args )
     {
-    	CommandLineArgs cla = new CommandLineArgs();
-    	JCommander jc = new JCommander(cla);
-    	jc.setProgramName("randomizer");
-    	jc.parse(args);
-    	
-    	// Print usage
-    	if (args.length < 2 || cla.help) {
-    		jc.usage();
+    	try {
+    		CommandLineArgs cla = new CommandLineArgs();
+        	JCommander jc = new JCommander(cla);
+        	jc.setProgramName("randomizer");
+        	jc.parse(args);
+        	
+        	// Print usage
+        	if (args.length < 2 || cla.help) {
+        		jc.usage();
+        	}
+        	
+        	// Run randomizer using provided seed value
+        	else if (cla.getSeed() != null) {
+        		Randomizer randomizer = new Randomizer();
+        		randomizer.randomize(cla.getSeed());
+        	}
     	}
-    	
-    	// Run randomizer using provided seed value
-    	else if (cla.getSeed() != null) {
-    		Randomizer randomizer = new Randomizer();
-    		randomizer.randomize(cla.getSeed());
+    	catch (Exception e) {
+    		exitWithError(e.getMessage());
     	}
     }
+    
+    public static void exitWithError(String msg) {
+    	System.err.println(msg);
+    	usage();
+    	System.exit(1);
+    }
+    
+    private static void usage() {
+    	JCommander jc = new JCommander(new CommandLineArgs());
+    	jc.usage();
+    }
+    
+    
 }
