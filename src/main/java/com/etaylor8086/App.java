dda4b5730dd5904a41b5f11a09c6e5c066243e2e
etaylor8086 <etaylor8086@gmail.com>
2017-05-28 21:43:45 -0700

    Create Maven quickstart project

---
+++ src/main/java/com/etaylor8086/App.java
@@ -0,0 +1,13 @@
+package com.etaylor8086;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
